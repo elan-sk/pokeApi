15d8749ea73236c31c9625e23c0059f3f25ce7cd
elan-sk <elan-sk@hotmail.com>
2024-12-18 06:57:16 -0500

    FINAL RERAL

--- src/components/LoadingList/index.jsx
+++ src/components/LoadingList/index.jsx
@@ -4,13 +4,16 @@ export default function LoadingList() {
   const classComponent = 'LoadingList'
   const classes = [
     classComponent,
+    'AllPokemon',
   ].join(' ')
   return (
     <div className={classes}>
       {/* From Uiverse.io by doniaskima  */}
-      <div className={`${classComponent}__loader`}></div>
-      <div className={`${classComponent}__message`}>
-        Loading...
+      <div>
+        <div className={`${classComponent}__loader`}></div>
+        <div className={`${classComponent}__message`}>
+          Loading...
+        </div>
       </div>
     </div>
   )
